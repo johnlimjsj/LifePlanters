48ac134181d1a59c42f6acea554f11dc4e95b28f
johnlimjsj <johnlimjsj@gmail.com>
2016-09-29 11:57:37 +0800

    hhh

--- lib/LifePlantersLib/LifePlanters.cpp
+++ lib/LifePlantersLib/LifePlanters.cpp
@@ -10,7 +10,7 @@ RunningAverage runavg_temp(5);
 Servo myfeeder;
 
 /******************************
-Constructors. ggggg
+Constructors. hhh
 ******************************/
 	LifePlanters::LifePlanters(uint8_t addr) : soilSensor(addr) { // calling another object constructor with my constructor
 	}
